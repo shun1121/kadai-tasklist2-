33908d36dd5caf8c1aec27bc9d9d40cd791f9193
shunsuke.fukunishi <hikitanikun.ware80000@gmail.com>
2020-07-31 08:52:02 +0000

    second task for lesson13

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,13 +10,15 @@
           <thead>
               <tr>
                   <th>id</th>
-                  <td>タスク</td>
+                  <th>ステータス</th>
+                  <th>タスク</th>
               </tr>
           </thead>
           <tbody>
               @foreach ($tasks as $task)
               <tr>
-                  <th>{{ link_to_route('tasks.show', $task->id, ['task' => $task->id]) }}</th>
+                  <td>{{ link_to_route('tasks.show', $task->id, ['task' => $task->id]) }}</td>
+                  <td>{{ $task->status }}</td>
                   <td>{{ $task->content }}</td>
               </tr>
               @endforeach
