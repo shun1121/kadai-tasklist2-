3ce7592247f378ed3f85c3144188dac97fad5261
shunsuke.fukunishi <hikitanikun.ware80000@gmail.com>
2020-07-31 03:22:09 +0000

    about to finish

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+<h1>タスク一覧</h>
+    
+    @if (count($tasks) > 0)
+      <table class="table table-striped">
+          <thead>
+              <tr>
+                  <th>id</th>
+                  <td>タスク</td>
+              </tr>
+          </thead>
+          <tbodey>
+              <tr>
+                  @foreach ($tasks as $task)
+                  <th>{{ link_to_route('tasks.show', $task->id, ['task' => $task->id]) }}</th>
+                  <td>{{ $task->content }}</td>
+                  @endforeach
+              </tr>
+          </tbodey>
+      </table>
+    @endif
+
+    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
+
+@endsection
